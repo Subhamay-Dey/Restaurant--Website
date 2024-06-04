2fa7c628bf95890ccf494d9e0a71b237e1e457da
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-04 21:02:12 +0530

    reveal function added and yeah my animatiion is finally completed and its working as fuck.

--- restaurant-website/src/components/Loader.jsx
+++ restaurant-website/src/components/Loader.jsx
@@ -1,50 +1,81 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-import {gsap, CSSPlugin, Expo} from "gsap";
+import { gsap, CSSPlugin, Expo } from "gsap";
 gsap.registerPlugin(CSSPlugin);
 
-export default function Loader() {
+function App() {
+  const [counter, setCounter] = useState(0);
 
-    const [counter, setCounter] = React.useState(0);
+  useEffect(() => {
+    const count = setInterval(() => {
+      setCounter((counter) =>
+        counter < 100
+          ? counter + 1
+          : (clearInterval(count), setCounter(100), reveal())
+      );
+    }, 25);
+  }, []);
 
-    React.useEffect(() => {
-        const count = setInterval(() => {
-          setCounter((counter) =>
-            counter < 100
-              ? counter + 1
-              : (clearInterval(count), setCounter(100))
-          );
-        }, 25);
-      }, []);
-    
+  const reveal = () => {
+    const t1 = gsap.timeline({
+      onComplete: () => {
+        console.log("completed");
+      },
+    });
+    t1.to(".follow", {
+      width: "100%",
+      ease: Expo.easeInOut,
+      duration: 1.2,
+      delay: 0.7,
+    })
+      .to(".hide", { opacity: 0, duration: 0.3 })
+      .to(".hide", { display: "none", duration: 0.3 })
+      .to(".follow", {
+        height: "100%",
+        ease: Expo.easeInOut,
+        duration: 0.7,
+        delay: 0.5,
+      })
+      .to(".content", { width: "100%", ease: Expo.easeInOut, duration: 0.7 })
+      .to(".title-lines", { display: "block", duration: 0.1 })
+      .to(".title-lines", {
+        opacity: 1,
+        stagger: 0.15,
+        ease: Expo.easeInOut,
+        duration: 0.6,
+      });
+  };
 
-  return <AppContainer>
+  return (
+    <AppContainer>
+      <Loading>
+        <Follow className="follow"></Follow>
+        <ProgressBar
+          className="hide"
+          id="progress-bar"
+          style={{ width: counter + "%" }}
+        ></ProgressBar>
+        <Count id="count" className="hide">
+          {counter}%
+        </Count>
+      </Loading>
 
-        <Loading>
-            <Follow className="follow"></Follow>
-            <ProgressBar className="hide" style={{width: counter + "%"}}></ProgressBar>
-            <Count className="hide">{counter}%</Count>
-        </Loading>
-
-        <Content>
-        <p className="title-lines">The greatest glory in living lies</p>
-        <p className="title-lines">not in never falling,</p>
-        <p className="title-lines">but in rising every time we fall.</p>
-        <p className="title-lines">-Nelson Mandela</p>
-        </Content>
-
+      <Content className="content">
+        <p className="title-lines">Fuck You</p>
+      </Content>
     </AppContainer>
+  );
 }
 
-const AppContainer = styled.div`
-    width: 100vw;
-    height: 100vw;
-    color: #000000;
-    position: relative;
-    text-align: center;
-`;
+export default App;
 
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  color: #000000;
+  position: relative;
+`;
 const Loading = styled.div`
   height: 100%;
   width: 100%;
@@ -55,7 +86,6 @@ const Loading = styled.div`
   position: absolute;
   top: 0;
 `;
-
 const Follow = styled.div`
   position: absolute;
   background-color: #f48049;
@@ -97,5 +127,14 @@ const Content = styled.div`
   justify-content: center;
   flex-direction: column;
   overflow: hidden;
-  color: #fff
+  color: #fff;
+
+  p {
+    text-align: center;
+    font-size: 104px;
+    opacity: 0;
+    display: none;
+    font-weight: 500;
+    margin: 0;
+  }
 `;
