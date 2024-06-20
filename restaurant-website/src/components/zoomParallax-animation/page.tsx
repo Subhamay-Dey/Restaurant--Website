0984296a8c0975efa881593800877fa6850a924d
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-20 23:24:47 +0530

    successfully completed the animation and also added smooth scrolling with lenis.

--- restaurant-website/src/components/zoomParallax-animation/page.tsx
+++ restaurant-website/src/components/zoomParallax-animation/page.tsx
@@ -1,8 +1,24 @@
+'use client'
+
 import React from 'react';
 import styles from "./style.module.scss"
 import Main from './main/index';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis'
 
 function ZoomParallax() {
+
+  useEffect( () => {
+    const lenis = new Lenis()
+   
+    function raf(time: number) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+    },[])
+
   return (
     <main className={styles.main}>
       <Main/>
