ab9f765985a38fbc462496db6c5f9ed937cc2f96
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-20 22:15:18 +0530

    made an array of pictures with their source and their animation scale and mapped it in the return code where evry picture is coming with their respectuce src, scale and index.

--- restaurant-website/src/components/zoomParallax-animation/main/index.jsx
+++ restaurant-website/src/components/zoomParallax-animation/main/index.jsx
@@ -3,6 +3,13 @@ import React, { useRef } from 'react';
 import styles from "./style.module.scss";
 import Image from 'next/image';
 import picture1 from "../../../../public/images/picture1.jpeg"
+import picture2 from "../../../../public/images/picture2.jpeg"
+import picture3 from "../../../../public/images/picture3.jpg"
+import picture4 from "../../../../public/images/picture4.jpg"
+import picture5 from "../../../../public/images/picture5.jpg"
+import picture6 from "../../../../public/images/picture6.jpg"
+import picture7 from "../../../../public/images/picture7.jpeg"
+
 import { useScroll, useTransform, motion } from 'framer-motion';
 
 function Main() {
@@ -14,16 +21,49 @@ const {scrollYProgress} = useScroll({
   offset: ['start start', 'end end']
 })
 
-  const scale4 = useTransform(scrollYProgress, [0, 1], [0, 4])
+  const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4])
+  const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5])
+  const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6])
+  const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8])
+  const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9])
+
+  const pictures = [
+    {
+      src: picture1, scale: scale4
+    },
+    {
+      src: picture2, scale: scale4
+    },
+    {
+      src: picture3, scale: scale4
+    },
+    {
+      src: picture4, scale: scale4
+    },
+    {
+      src: picture5, scale: scale4
+    },
+    {
+      src: picture6, scale: scale4
+    },
+    {
+      src: picture7, scale: scale4
+    }
+  ]
 
   return (
     <div ref={container} className={styles.container}>
       <div className={styles.sticky}>
-        <div className={styles.el}>
-          <motion.div style={{scale: scale4}} className={styles.imageContainer}>
-            <Image src={picture1} fill alt="image" placeholder='blur'/>
-          </motion.div>
-        </div>
+        {
+          pictures.map(({src, scale}, index) => {
+            return <div key={index} className={styles.el}>
+            <motion.div style={{scale}} className={styles.imageContainer}>
+              <Image src={src} fill alt="image" placeholder='blur'/>
+            </motion.div>
+          </div>
+          })
+        }
+        
       </div>
     </div>
   )
