98cb41bd1d67656abd2561fb27ab50470965e238
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-20 20:32:38 +0530

    added all the images in the public and updated index.tsx

--- restaurant-website/src/components/zoomParallax-animation/main/index.tsx
+++ restaurant-website/src/components/zoomParallax-animation/main/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from "./style.module.scss";
 import Image from 'next/image';
+import picture1 from "../../../../public/images/picture1.jpeg"
 
 function Main() {
   return (
     <div className={styles.container}>
-      <div className={styles.sticky}></div>
+      <div className={styles.sticky}>
+        <div className={styles.el}>
+          <div className={styles.imageContainer}>
+            <Image src={picture1} fill alt="image" placeholder='blur'/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
