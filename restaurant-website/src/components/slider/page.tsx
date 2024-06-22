9406f57a49625f18a555b1d9dc411efc114787b1
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-22 18:40:35 +0530

    completed another animation which is the slider animation.

--- restaurant-website/src/components/slider/page.tsx
+++ restaurant-website/src/components/slider/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import styles from "./style.module.scss"
+
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-import 'swiper/swiper-bundle.min.css';
+// import 'swiper/swiper-bundle.min.css';
 
 import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
 
@@ -20,63 +22,63 @@ import slide_img4 from "../../../public/images/img_4.jpg";
 import slide_img5 from "../../../public/images/img_5.jpg";
 import slide_img6 from "../../../public/images/img_6.jpg";
 import slide_img7 from "../../../public/images/img_7.jpg";
-import Image from 'next/image';
-
 
 function Slider() {
   return (
     <div className="container">
-      <h1 className='heading'>Slider</h1>
+      <h1 className="heading">Flower Gallery</h1>
       <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={
-          {
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2.5,
-          }
-        }
-        className='swiper_container'
+        effect="coverflow"
+        grabCursor
+        centeredSlides
+        loop
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          // clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
       >
         <SwiperSlide>
-          <Image src={slide_img1} alt='slide_image'/>
+          <img src={slide_img1.src} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_img2.src} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_img3.src} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_img4.src} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_img5.src} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_img6.src} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_img7.src} alt="slide_image" />
         </SwiperSlide>
 
-        <SwiperSlide>
-          <Image src={slide_img2} alt='slide_image'/>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Image src={slide_img3} alt='slide_image'/>
-        </SwiperSlide>
-        
-        <SwiperSlide>
-          <Image src={slide_img4} alt='slide_image'/>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Image src={slide_img5} alt='slide_image'/>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Image src={slide_img6} alt='slide_image'/>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Image src={slide_img7} alt='slide_image'/>
-        </SwiperSlide>
-
-        <div className="slider-controller">
-          <div className="swiper-button-prev">
-            <ion-icon name=""></ion-icon>
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <IonIcon name="arrow-back-outline" />
           </div>
+          <div className="swiper-button-next slider-arrow">
+            <IonIcon name="arrow-forward-outline" />
+          </div>
+          <div className="swiper-pagination" />
         </div>
-
       </Swiper>
     </div>
   )
